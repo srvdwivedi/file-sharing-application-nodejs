efe5395e555d5e0e8c409659f9ee2631724f7721
srv.dwivedi <sourav.dwivedi@auw.co.in>
2021-01-19 22:51:11 +0530

    updated env

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 function connectDB() {
     // DB connection
-    mongoose.connect('mongodb+srv://souravdwivedi:admin@123@cluster0.0efip.mongodb.net/test?retryWrites=true&w=majority' , { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+    mongoose.connect(url , { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
     const connection = mongoose.connection;
     connection.once('open', () => {
       console.log("MongoDB database connection established successfully");
