6baaccbe1d2a8734792b2e19b7cbaecdd4ac6f36
srv.dwivedi <sourav.dwivedi@auw.co.in>
2021-01-24 20:31:24 +0530

    added endpoints to upload file

--- routes/files.js
+++ routes/files.js
@@ -1,6 +1,46 @@
-const router = require('express').Router;
+const router = require("express").Router;
+const multer = require("multer");
+const path = require("path");
+const File = require('../models/file');
+const {v4 : uuid4} = require('uuid'); 
 
-router.post('/api/files');
+let storage = multer.diskStorage({
+  destination: (req, file, callback) => callback(null, "./uploads/"),
+  filename: (req, file, callback) => {
+    const name = `${Date.now()}-${Math.round(
+      Math.random() * 1e4
+    )}${path.extname(file, originalname)}`;
+    callback(null, name);
+  },
+});
 
- 
+let upload = multer({
+  storage,
+  limit: { fileSize: 1000000 * 100 },
+}).single("");
+
+router.post("/", (req, res) => {
+    if (!req.file) {
+        return res.json({ error: "Please add your file " });
+    }
+
+    upload(req, res, async (err) => {
+    if (err) {
+        return res.status(500).send({ error: err.message });
+    }
+
+    const file = new File({
+        filename : req.file.filename,
+        uuid : uuid4(),
+        path : req.file.path,
+        size : req.file.size 
+    });
+
+    const response = await file.save();
+    return res.json({file : `${process.env.APP_BASE_URL}/files/${response.uuid}`});  
+    
+    });
+
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+
 const PORT = process.env.PORT;
 
 const connectDB = require("./config/db");
 connectDB();
+
+
+app.use('api/files', require('./routes/files')); 
+
 
 app.listen(PORT, ()=> {
     console.log(`Running on port : ${PORT}`);
